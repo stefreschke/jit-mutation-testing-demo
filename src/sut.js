35dd5c5ff006443f614cd2469e5c52ba135c2498
Stefan Reschke <stefan.reschke@outlook.de>
2022-11-19 12:17:45 +0100

    Add simple tests for adhoc polymorph confusion of mutation testing

--- src/sut.js
+++ src/sut.js
@@ -10,3 +10,26 @@ module.exports.somePredicate = function (number) {
 module.exports.additionOfNumbersAndStrings = function (a, b) {
     return a + b
 }
+
+function test_somePredicate_16ShouldBeFalse() {
+    let result = sut.somePredicate(16)
+    if (result) throw Error()
+}
+
+function test_somePredicate_8ShouldBeTrue() {
+    let result = sut.somePredicate(8)
+    if (!result) throw Error()
+}
+
+function test_additionOfNumbersAndStrings_numbers() {
+    let result = sut.additionOfNumbersAndStrings("a", "b")
+    if (result != "ab") throw Error()
+    assert.equal(result, "ab")
+}
+
+function test_additionOfNumbersAndStrings_strings() {
+    let result = sut.additionOfNumbersAndStrings(4, 5)
+    if (result != 9) throw Error()
+}
+
+console.log("bc189ee8-5c09-4d30-9220-09a40ac35194")
